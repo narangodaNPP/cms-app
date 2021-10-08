027f201a093c4d619719623a9f531f189d7a1997
narangodaNPP <narangodapasan@gmail.com>
2021-10-09 04:51:33 +0530

    landing window struture

---
+++ cms-app/Views/landingWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cms_app.Views
+{
+    /// <summary>
+    /// Interaction logic for landingWindow.xaml
+    /// </summary>
+    public partial class landingWindow : Window
+    {
+        public landingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
