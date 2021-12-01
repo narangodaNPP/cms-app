7f04f1295c9409082e0d5b30311e5414a1e1f258
Pasan Narangoda <narangodapasan@gmail.com>
2021-12-01 06:02:24 +0530

    create student and payment models

---
+++ cms-app/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cms_app.Models
+{
+    class Student
+    {
+        [Key]
+        public string studentID { get; set; }
+
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public int contactNo { get; set; }
+
+        [Required]
+        public string password { get; set; }
+        public int age { get; set; }
+    }
+}
