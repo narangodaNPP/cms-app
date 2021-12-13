2a361d65765071bd693d4c7331a3dfb8e6ec9f43
AchilaSR <achilasandeep@gmail.com>
2021-12-14 03:35:00 +0530

    create teacher model

---
+++ cms-app/Models/teacher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cms_app.Models
+{
+   public class teacher
+    {
+        [Key]
+        public string teacherID { get; set; }
+
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public int contactNo { get; set; }
+
+        [Required]
+        public string password { get; set; }
+        public int age { get; set; }
+        public string subject { get; set; }
+    }
+}
+
