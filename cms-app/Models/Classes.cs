607af94fdf3e25ca4ab7ccdc2a1437c38b91319c
AchilaSR <achilasandeep@gmail.com>
2021-12-14 04:06:45 +0530

    create classes model

---
+++ cms-app/Models/Classes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cms_app.Models
+{
+    class Classes
+    {
+        [Key]
+        public string classID { get; set; }
+        
+        [Required]
+        public string subject { get; set; }
+
+        [Required]
+        public string teacher { get; set; }
+
+        [Required]
+        public string date { get; set; }
+
+        [Required]
+        public string time { get; set; }
+
+
+
+    }
+}
