474ed4183c0e6992fb81609016d8230f9d148980
AchilaSR <achilasandeep@gmail.com>
2021-12-16 02:15:35 +0530

    teacher signup back

--- cms-app/Views/DatabaseReposi.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace cms_app.Views
-{
-    internal class DatabaseReposi
-    {
-        internal object classes_set;
-    }
-}
--- cms-app/Views/teacherSignUp.xaml.cs
+++ cms-app/Views/teacherSignUp.xaml.cs
@@ -38,7 +38,8 @@ namespace cms_app.Views
             contactNo = Convert.ToInt32(txtcontactno.Text),
             password = password.Password,
             dateofbirth = dateofbirth.SelectedDate.Value,
-            
+            subject = cboxSubject.Text,
+
         };
         repository.teachers.Add(Teacher);
         repository.SaveChanges();
